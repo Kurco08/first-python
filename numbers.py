626aa5bb5e34a6cdca947c64666c16de7cc5acba
Kurco08 <gofighters22@gmail.com>
2019-08-22 18:45:45 -0300

    Test

---
+++ numbers.py
@@ -0,0 +1,5 @@
+from roma_numbers import roman_to_decimal
+if __name__ == "__main__":
+    print("test 1:" + str(roman_to_decimal('I')))
+    print("test 2:" + str(roman_to_decimal('II')))
+    print("test 3:" + str(roman_to_decimal('III')))
